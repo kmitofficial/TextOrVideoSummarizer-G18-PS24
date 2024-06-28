5437b8962c3705a2f071ade15a125626efa02bbf
Durga-prasad-1 <147145773+Durga-prasad-1@users.noreply.github.com>
2024-06-28 15:38:03 +0530

    Update pe.py

--- WebApp/pages/pe.py
+++ WebApp/pages/pe.py
@@ -1,46 +1,39 @@
 import os
 import streamlit as st
-import google_auth_oauthlib.flow
 import googleapiclient.discovery
 import googleapiclient.errors
 
-scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
-
-
-
 def main():
+    api_key = "AIzaSyCYUSC2ZeccGO7ax4FyETv6aSADU-fortU"  # Replace with your API key
     channelMail = st.text_input("Enter channel mail")
-    # Disable OAuthlib's HTTPS verification when running locally.
-    # *DO NOT* leave this option enabled in production.
-    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
     api_service_name = "youtube"
     api_version = "v3"
-    client_secrets_file = "desktop.json"
 
-    # Get credentials and create an API client
-    if(st.button("Enter")):
-        flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-            client_secrets_file, scopes)
-        credentials = flow.run_local_server(port=0)
+    # Create an API client
+    if st.button("Enter"):
         youtube = googleapiclient.discovery.build(
-            api_service_name, api_version, credentials=credentials)
+            api_service_name, api_version, developerKey=api_key)
 
         request = youtube.channels().list(
             part="snippet,contentDetails,statistics",
-            forHandle=channelMail
+            forHandle=channelMail  
         )
         response = request.execute()
-        channelID = response["items"][0]["id"]
-        request = youtube.search().list(
-            part="snippet",
-            channelId=channelID,
-            maxResults=1,
-            order="date"
-        )
-        response = request.execute()
-        st.write(response["items"][0]["id"]["videoId"]);
-        print(response["items"][0]["id"]["videoId"])
+        
+        if "items" in response and len(response["items"]) > 0:
+            channelID = response["items"][0]["id"]
+            request = youtube.search().list(
+                part="snippet",
+                channelId=channelID,
+                maxResults=1,
+                order="date"
+            )
+            response = request.execute()
+            st.write(response["items"][0]["id"]["videoId"])
+            print(response["items"][0]["id"]["videoId"])
+        else:
+            st.write("No channel found for the provided email.")
 
 if __name__ == "__main__":
     main()
