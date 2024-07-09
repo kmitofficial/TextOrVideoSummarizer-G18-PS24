4e3f69878f5e6da12282eb21e42ae711538a8fec
Jayanth-137 <jayanth.karnati2003@gmail.com>
2024-07-09 15:50:16 +0530

    new file:   WebApp/.env 	modified:   WebApp/load.py 	modified:   WebApp/requirements.txt

--- WebApp/load.py
+++ WebApp/load.py
@@ -1,7 +1,10 @@
 import google.generativeai as genai
+import os
+from dotenv import load_dotenv
 def output(text):
     response = model.generate_content("Summarize this and also don't make this too short "+text)
     return response
-GOOGLE_API_KEY="AIzaSyCgX2dUhfUNGZ1aSrjUIlXNfor1ylBdw7I"
+load_dotenv()
+GOOGLE_API_KEY = os.getenv("API_KEY")
 genai.configure(api_key=GOOGLE_API_KEY)
 model = genai.GenerativeModel('gemini-pro')
